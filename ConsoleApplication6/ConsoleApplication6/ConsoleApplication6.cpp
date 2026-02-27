5397ccbdb9c86ffdabc23e5f1785e3595d6066a6
H0vak <novak.anton2k8@gmail.com>
2026-02-27 20:57:16 +0200

    Add files via upload

---
+++ ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int n, V;
+    cout << "Введіть n та V: ";
+    cin >> n >> V;
+
+    int cost = 0;
+    int fuel = 0;
+
+    int buy_first = min(V, n - 1);
+    cost += buy_first * 1;
+    fuel = buy_first;
+
+    for (int city = 2; city < n; city++) {
+        fuel--;
+
+        if (fuel < n - city) {
+            cost += city;
+            fuel++;
+        }
+    }
+
+    cout << "Мінімальна вартість: " << cost;
+    return 0;
+}
